bc3d58c7c033016f384d9c2942000576c0483923
Johannes Drever <johannes.drever@linova.de>
2013-12-07 19:04:32 +0100

    redirect to answer.

--- trial.php
+++ trial.php
@@ -11,21 +11,25 @@
         <script>
 <?php 
         $mu = (2 * rand(0, 1)) - 1;
+        $a = myRand(0, 1);
+        $k = myRand(0, 1);
         echo "var mu = ".$mu.";";
-        echo "var A = ".myRand(0, 1).";";
+        echo "var A = ".$a.";";
         echo "var B = -0.025;";
         echo "var dt = 0.01;";
         echo "var omega = 1.6 * 2 * 3.142;";
         echo "var alpha = 0.641;";
         echo "var beta = 0.05;";
         echo "var gamma = 12.457;";
-        echo "var k = ".myRand(0, 1).";";
+        echo "var k = ".$k.";";
         ?>
         </script>
         <script>
-           <?php
-           echo "var timer = setInterval(function(){alert('Hello')},3000);";
-        ?>
+        var timer = setInterval(trialEnd,3000);
+
+        function trialEnd(){
+           <?php echo "window.location = 'answer.php?mu=".$mu."&k=".$k."&a=".$a."';" ?>
+        }
         </script>
            <script src="js/vpi.js"></script>
        </div>
