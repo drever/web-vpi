2eeae4889f53d189566fc44693f186386b3c88d1
deep-introspection <deep@introspection.eu>
2013-12-07 11:40:07 -0500

    Change CSS

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Welcome to Web-VPI</title>
-<link href="../style_voyant2.css" rel="stylesheet" type="text/css" />
+	<title>Welcome to Web-VPI</title>
+	<link href="css/vpi.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
